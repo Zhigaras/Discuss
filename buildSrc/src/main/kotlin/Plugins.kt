42c11e6e7af8e0b8c4f0a4758b5f8c3d32e0bd91
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-08-03 03:42:42 +0600

    dependencies management added

---
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -0,0 +1,10 @@
+object Plugins {
+    
+    const val application = "com.android.application"
+    const val android = "org.jetbrains.kotlin.android"
+    const val library = "com.android.library"
+    const val googleServices = "com.google.gms.google-services"
+    const val crashlytics = "com.google.firebase.crashlytics"
+    const val kapt = "kotlin-kapt" //TODO remove?
+    
+}
