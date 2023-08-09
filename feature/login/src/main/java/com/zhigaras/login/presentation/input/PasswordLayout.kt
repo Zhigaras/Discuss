905e546ffeaa44bc8b9ea232a567e189e45c39a6
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-08-10 02:18:53 +0600

    email state saving issue fixed

--- feature/login/src/main/java/com/zhigaras/login/presentation/input/PasswordLayout.kt
+++ feature/login/src/main/java/com/zhigaras/login/presentation/input/PasswordLayout.kt
@@ -5,7 +5,7 @@ import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import androidx.core.view.isVisible
+import com.zhigaras.login.R
 import com.zhigaras.login.databinding.PasswordLayoutBinding
 
 class PasswordLayout @JvmOverloads constructor(
@@ -15,7 +15,7 @@ class PasswordLayout @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr), InputValidation {
     
     private val binding = PasswordLayoutBinding.inflate(LayoutInflater.from(context))
-    
+    private val errorTextId = R.string.password_matching_error
     private val passwordLayout = binding.inputPassword.root
     private val confirmPasswordLayout = binding.confirmPassword.root
     private val errorView = binding.passwordMismatchErrorView
@@ -27,22 +27,18 @@ class PasswordLayout @JvmOverloads constructor(
     
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        addTextWatcher(AuthTextWatcher { errorView.visibility = INVISIBLE })
+        addTextWatcher(AuthTextWatcher { errorView.text = "" })
     }
     
     override fun isValid(): Boolean {
-        return innerIsValid()
+        val isPasswordsEquals = passwordLayout.text() == confirmPasswordLayout.text()
+        errorView.text = if (isPasswordsEquals) "" else context.getText(errorTextId)
+        val passListValidation = passwordList.map { it.isValid() }
+        return passListValidation.all { it } && isPasswordsEquals
     }
     
     override fun addTextWatcher(textWatcher: TextWatcher) {
         passwordList.forEach { it.addTextWatcher(textWatcher) }
-    }
-    
-    private fun innerIsValid(): Boolean {
-        val isPasswordsEquals = passwordLayout.text() == confirmPasswordLayout.text()
-        errorView.isVisible = !isPasswordsEquals
-        val passListValidation = passwordList.map { it.isValid() }
-        return passListValidation.all { it } && isPasswordsEquals
     }
     
     fun text(): String {
