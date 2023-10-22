a8f312bcb1c21f79a75c959fc883ce1ec0d9aafd
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-10-23 02:08:31 +0600

    messaging layouts configurations created, koin viewModel providing refactored in custom views

---
+++ feature/messaging/src/main/java/com/zhigaras/messaging/ui/LandscapeMessagesLayout.kt
@@ -0,0 +1,51 @@
+package com.zhigaras.messaging.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.isVisible
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+import com.zhigaras.adapterdelegate.CompositeAdapter
+import com.zhigaras.core.viewModel
+import com.zhigaras.messaging.R
+
+open class LandscapeMessagesLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+    
+    private val messagesRv: RecyclerView by lazy { findViewById(R.id.messages_rv) }
+    private val editText: AppCompatEditText by lazy { findViewById(R.id.new_message_edit_text) }
+    private val sendButton: ImageView by lazy { findViewById(R.id.send_message_button) }
+    
+    private val viewModel: MessagesViewModel by viewModel()
+    
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        val adapter = CompositeAdapter.Builder()
+            .addAdapter(IncomingMessageAdapter())
+            .addAdapter(OutgoingMessageAdapter())
+            .build()
+        messagesRv.adapter = adapter
+        viewModel.observe(findViewTreeLifecycleOwner() ?: return) {
+            it.handle(adapter)
+        }
+        val textWatcher = MessageTextWatcher {
+            sendButton.isVisible = it.isNotBlank()
+        }
+        editText.addTextChangedListener(textWatcher)
+        sendButton.setOnClickListener {
+            val msg = editText.text
+            if (msg.isNullOrBlank()) return@setOnClickListener
+            else {
+                viewModel.sendMessage(msg.trim().toString())
+                editText.text = null
+                messagesRv.smoothScrollToPosition(adapter.itemCount)
+            }
+        }
+    }
+}
--- feature/messaging/src/main/java/com/zhigaras/messaging/ui/PortraitMessagesLayout.kt
+++ feature/messaging/src/main/java/com/zhigaras/messaging/ui/PortraitMessagesLayout.kt
@@ -5,31 +5,23 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageView
 import android.widget.LinearLayout
-import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.isVisible
 import androidx.lifecycle.findViewTreeLifecycleOwner
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.RecyclerView
-import com.zhigaras.adapterdelegate.CompositeAdapter
 import com.zhigaras.messaging.R
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import org.koin.java.KoinJavaComponent.inject
 
-class MessagesLayout @JvmOverloads constructor(
+class PortraitMessagesLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+) : LandscapeMessagesLayout(context, attrs, defStyleAttr) {
     
     private val showHideButton: ImageView by lazy { findViewById(R.id.show_hide_button) }
-    private val messagesRv: RecyclerView by lazy { findViewById(R.id.messages_rv) }
-    private val editText: AppCompatEditText by lazy { findViewById(R.id.new_message_edit_text) }
-    private val sendButton: ImageView by lazy { findViewById(R.id.send_message_button) }
     private val layoutToHide: LinearLayout by lazy { findViewById(R.id.layout_to_hide) }
     
     private var isExpanded = MutableStateFlow(false)
-    private val viewModel: MessagesViewModel by inject(MessagesViewModel::class.java)
     
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val maxHeightMeasureSpec = MeasureSpec.makeMeasureSpec(800, MeasureSpec.AT_MOST)
@@ -37,7 +29,6 @@ class MessagesLayout @JvmOverloads constructor(
     }
     
     override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
         layoutTransition = LayoutTransition()
         findViewTreeLifecycleOwner()?.lifecycleScope?.launch {
             isExpanded.collect {
@@ -51,26 +42,6 @@ class MessagesLayout @JvmOverloads constructor(
         showHideButton.setOnClickListener {
             isExpanded.value = !isExpanded.value
         }
-        val adapter = CompositeAdapter.Builder()
-            .addAdapter(IncomingMessageAdapter())
-            .addAdapter(OutgoingMessageAdapter())
-            .build()
-        messagesRv.adapter = adapter
-        viewModel.observe(findViewTreeLifecycleOwner() ?: return) {
-            it.handle(adapter)
-        }
-        val textWatcher = MessageTextWatcher {
-            sendButton.isVisible = it.isNotBlank()
-        }
-        editText.addTextChangedListener(textWatcher)
-        sendButton.setOnClickListener {
-            val msg = editText.text
-            if (msg.isNullOrBlank()) return@setOnClickListener
-            else {
-                viewModel.sendMessage(msg.trim().toString())
-                editText.text = null
-                messagesRv.smoothScrollToPosition(adapter.itemCount)
-            }
-        }
+        super.onAttachedToWindow()
     }
 }
