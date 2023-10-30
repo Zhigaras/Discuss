bf13663ca07a1a910d3876efc56b624096bacbef
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-10-30 17:23:38 +0600

    removing callback implemented in HomeViewModel

--- feature/home/src/main/java/com/zhigaras/home/domain/HomeCloudService.kt
+++ feature/home/src/main/java/com/zhigaras/home/domain/HomeCloudService.kt
@@ -6,4 +6,6 @@ import com.zhigaras.home.domain.model.HomeSubject
 interface HomeCloudService {
     
     fun subscribeToSubjects(callback: CloudService.Callback<List<HomeSubject>>)
+    
+    fun removeCallback(callback: CloudService.Callback<List<HomeSubject>>)
 }
