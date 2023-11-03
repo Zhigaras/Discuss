69afcdef832d765840b2ba44baa3b1dce39aa22f
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-04 01:05:28 +0600

    simple ConnectionStateView created

---
+++ app/src/main/java/com/zhigaras/discuss/ConnectionStateView.kt
@@ -0,0 +1,23 @@
+package com.zhigaras.discuss
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class ConnectionStateView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+    
+    fun showConnectionLost() {
+        visibility = VISIBLE
+        setBackgroundColor(context.getColor(com.zhigaras.messaging.R.color.dark_red))
+        setText(R.string.connection_lost)
+    }
+    
+    fun hideConnectionLost() {
+        setBackgroundColor(context.getColor(com.zhigaras.messaging.R.color.light_green))
+        setText(R.string.connection_restored)
+    }
+}
