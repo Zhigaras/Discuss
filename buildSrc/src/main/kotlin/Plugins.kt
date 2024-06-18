315c6ad89caf0a4e6e01a826ceec2d7e701b20da
Жигарас <zhigaras.dev@gmail.com>
2024-06-19 01:54:17 +0500

    migrate to gradle build scripts

--- buildSrc/src/main/kotlin/Plugins.kt
+++
@@ -1,9 +0,0 @@
-object Plugins {
-    
-    const val application = "com.android.application"
-    const val android = "org.jetbrains.kotlin.android"
-    const val library = "com.android.library"
-    const val googleServices = "com.google.gms.google-services"
-    const val crashlytics = "com.google.firebase.crashlytics"
-    const val parcelize = "kotlin-parcelize"
-}
